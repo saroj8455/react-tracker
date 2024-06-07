b0960081069d0900f2fddd61d9f007343561a889
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-06-07 22:49:59 +0530

    update basic css and react-router

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import 'primereact/resources/themes/lara-light-cyan/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
