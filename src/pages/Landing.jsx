2ce2eda018638dc8e487e68775f3f3e006f4dc6c
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-06-07 23:30:47 +0530

    customize landing heading

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Landing() {
   return (
     <div className='min-h-screen flex flex-column justify-content-center align-items-center'>
-      <h1 className='text-3xl text-orange-500'>Landing Page</h1>
+      <h1 className='text-8xl text-orange-500'>Landing Page</h1>
     </div>
   );
 }
