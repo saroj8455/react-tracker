72256683907697a49e5362ed78e9db5a071b8ad7
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-06-07 23:39:37 +0530

    access params docs

--- README.md
+++ README.md
@@ -38,3 +38,15 @@ Currently, two official plugins are available:
   <Outlet />
 </div>
 ```
+
+# Access params in children component
+
+```jsx
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Vue() {
+  const { vueId } = useParams();
+  return <div>Vue: {vueId}</div>;
+}
+```
--- src/pages/Vue.jsx
+++ src/pages/Vue.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import { useParams } from 'react-router-dom';
 export default function Vue() {
+  const { vueId } = useParams();
   return (
     <div className='min-h-screen flex flex-column justify-content-center align-items-center'>
-      <h1 className='text-3xl text-orange-500'>Vue Tech</h1>
+      <h1 className='text-3xl text-orange-500'>
+        Vue Tech : <span className='text-blue-500'>{vueId}</span>
+      </h1>
     </div>
   );
 }
